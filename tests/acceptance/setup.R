d3fd18333d3491a301fd719cdc70a0aaedc642da
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2025-05-26 10:46:16 +0200

    test: :white_check_mark: skip muttest on a package if downloading a repo failed

--- tests/acceptance/setup.R
+++ tests/acceptance/setup.R
@@ -2,7 +2,12 @@ get_repo <- function(url) {
   parts <- strsplit(url, "/")[[1]]
   zip <- parts[length(parts)]
   withr::with_dir(fs::path(tempdir()), {
-    download.file(url = url, destfile = zip, quiet = TRUE)
+    tryCatch(
+      download.file(url = url, destfile = zip, quiet = TRUE),
+      error = function(e) {
+        testthat::skip(paste("Failed to download repository from", url))
+      }
+    )
     path <- unzip(zipfile = zip)
   })
   fs::path(tempdir(), fs::path_common(path))
